4c46653891abd80d8ac6fb578dfb33dbef3ebb4f
Lucas Tjor <tjor75@gmail.com>
2025-06-13 19:13:46 -0300

    Convertir validaciones-helper.js en conjunto de funciones

--- src/modules/validaciones-helper.js
+++ src/modules/validaciones-helper.js
@@ -1,31 +1,29 @@
-class ValidacionesHelper {
-    getIntegerOrDefault = (value, defaultValue) => {
-        const int = parseInt(value);
-        return int !== NaN ? int : defaultValue;
-    };
-    getDateOrDefault = (value, defaultValue) => {
-        let date;
-        try {
-            date = new Date(value);
-            return date;
-        } catch (e) {
-            date = defaultValue;
-        } finally {
-            return date;
-        }
-    };
-    getStringOrDefault = (value, defaultValue) => {
-        return typeof value === 'string' ? value : defaultValue;
-    };
-    getBooleanOrDefault = (value, defaultValue) => {
-        const isTrue    = value.toLowerCase() === 'true'  || Number(value) === 1;
-        const isFalse   = value.toLowerCase() === 'false' || Number(value) === 0;
-        return isTrue || isFalse ? isTrue : defaultValue;
-    };
-    isEmail = (value) => {
-        const REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        return REGEX.test(value);
-    };
-}
+const getIntegerOrDefault = (value, defaultValue) => {
+    const int = parseInt(value);
+    return int !== NaN ? int : defaultValue;
+};
+const getDateOrDefault = (value, defaultValue) => {
+    let date;
+    try {
+        date = new Date(value);
+        return date;
+    } catch (e) {
+        date = defaultValue;
+    } finally {
+        return date;
+    }
+};
+const getStringOrDefault = (value, defaultValue) => {
+    return typeof value === 'string' ? value : defaultValue;
+};
+const getBooleanOrDefault = (value, defaultValue) => {
+    const isTrue    = value.toLowerCase() === 'true'  || Number(value) === 1;
+    const isFalse   = value.toLowerCase() === 'false' || Number(value) === 0;
+    return isTrue || isFalse ? isTrue : defaultValue;
+};
+const isEmail = (value) => {
+    const REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return REGEX.test(value);
+};
 
-export default new ValidacionesHelper();
+export { getIntegerOrDefault, getDateOrDefault, getStringOrDefault, getBooleanOrDefault, isEmail };
