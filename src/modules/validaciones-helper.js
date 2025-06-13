de0d84958a621fc028a043f403719a3d2460045f
Lucas Tjor <tjor75@gmail.com>
2025-06-13 19:10:31 -0300

    Llenar todos los metodos de validaciones-helper.js

--- src/modules/validaciones-helper.js
+++ src/modules/validaciones-helper.js
@@ -1,12 +1,31 @@
 class ValidacionesHelper {
     getIntegerOrDefault = (value, defaultValue) => {
         const int = parseInt(value);
-        return int ? int !== NaN : defaultValue;
+        return int !== NaN ? int : defaultValue;
     };
-    getDateOrDefault = (value, defaultValue) => { return null};
-    getStringOrDefault = (value, defaultValue) => { return null};
-    getBooleanOrDefault = (value, defaultValue) => { return null};
-    isEmail = (value) => { return null};
+    getDateOrDefault = (value, defaultValue) => {
+        let date;
+        try {
+            date = new Date(value);
+            return date;
+        } catch (e) {
+            date = defaultValue;
+        } finally {
+            return date;
+        }
+    };
+    getStringOrDefault = (value, defaultValue) => {
+        return typeof value === 'string' ? value : defaultValue;
+    };
+    getBooleanOrDefault = (value, defaultValue) => {
+        const isTrue    = value.toLowerCase() === 'true'  || Number(value) === 1;
+        const isFalse   = value.toLowerCase() === 'false' || Number(value) === 0;
+        return isTrue || isFalse ? isTrue : defaultValue;
+    };
+    isEmail = (value) => {
+        const REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return REGEX.test(value);
+    };
 }
 
 export default new ValidacionesHelper();
