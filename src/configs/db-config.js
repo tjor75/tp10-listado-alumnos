ac83284fd168fa0dcf25100682539547a7c59d52
Android Studio <devandroid@ort>
2025-06-30 11:16:10 -0300

    Adapatar configs/db-config.js a sugerencia de presentacion

--- src/configs/db-config.js
+++ src/configs/db-config.js
@@ -1,12 +1,12 @@
 let config;
 
-if (process.env.POSTGRES_STRING === undefined) {
+if (!process.env.POSTGRES_STRING) {
     config = {
-        host        : process.env.POSTGRES_HOST || "localhost",
-        database    : process.env.POSTGRES_DATABASE || "postgres",
-        user        : process.env.POSTGRES_USER || "postgres",
-        password    : process.env.POSTGRES_PASSWORD || "root",
-        port        : parseInt(process.env.POSTGRES_PORT) || 5432
+        host        : process.env.POSTGRES_HOST     ?? "",
+        database    : process.env.POSTGRES_DATABASE ?? "",
+        user        : process.env.POSTGRES_USER     ?? "",
+        password    : process.env.POSTGRES_PASSWORD ?? "",
+        port        : process.env.POSTGRES_PORT     ?? 5432
     };
 } else {
     config = { connectionString: process.env.POSTGRES_STRING };
